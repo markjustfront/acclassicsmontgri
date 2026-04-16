60a78514bf821dfed0a76a371a89c462e9604bd7
Mark Ayllon Jaime <mark.frontdevops@gmail.com>
2026-04-16 16:51:11 +0200

    wash 3.5.9

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,7 +89,9 @@ const modelsData = [
             {
                 title: "Bota de canvi de marxes del R5 en pell",
                 youtubeId: "CQthDNyU7f0",
-                description: "Explicació de la instal·lació d'una bota de canvi de marxes en pell per a un Renault 5."
+                description: "Explicació de la instal·lació d'una bota de canvi de marxes en pell per a un Renault 5.",
+                images: ["images/accessories/R5/bota-canvi-r5.jpg"],
+                description: "Aixo era un acesori molt raro de trobar, i es va vendre a les botigues oficials Renault durant els anys 80. Molt buscada pels col·leccionistes actualment.",
             }
         ]
     },
@@ -216,8 +218,8 @@ window.showModel = function (id) {
                 <summary>${acc.name}</summary>
                 <div class="details-content">
                     <p>${acc.description}</p>
-                    ${acc.images && acc.images.length ? 
-                        `<div class="accessory-images">
+                    ${acc.images && acc.images.length ?
+                `<div class="accessory-images">
                             ${acc.images.map(img => `<img src="${img}" alt="${acc.name}">`).join('')}
                          </div>` : ''}
                     ${acc.extra ? `<p><strong>Extra:</strong> ${acc.extra}</p>` : ''}
