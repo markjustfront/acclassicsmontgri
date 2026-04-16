22388a3c8bd4a246e9289e15790309e7da3fccf2
Mark Ayllon Jaime <mark.frontdevops@gmail.com>
2026-04-16 17:13:30 +0200

    wash 3.5.9.4

--- assets/js/script.js
+++ assets/js/script.js
@@ -114,7 +114,7 @@ const modelsData = [
         model: "5 Super Cinq (Phase 2)",
         years: "1984-1996",
         description: "La segona generació del Renault 5, coneguda com a Super Cinq. Disseny modernitzat i gamma més àmplia.",
-        image: "images/renault5-supercinq.jpg",   // ← Canvia la imatge si en tens una específica del Super Cinq
+        image: "images/renault5-supercinq.jpg",
 
         generalCharacteristics: `
             <strong>Període de producció:</strong> 1984-1996<br>
